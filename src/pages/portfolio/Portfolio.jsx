3fa0d2311b93058e26f18774a909d796876056fb
Drew-Andersen <andrewtandersen@gmail.com>
2024-09-03 15:23:05 -0400

    Fixed the image bug

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -4,24 +4,26 @@ import './portfolio.css';
 export default function Portfolio() {
     return (
         <>
-            <h1 className="text-center p-5">Portfolio</h1>
-            <div className="project-section w-100">
-                {projects.map((project, index) => {
-                    return (
-                        <div id={index} className="projects" key={index}>
-                            <a href={project.liveLink} target="_blank">
-                                <div className="project-buffer">
-                                    <div className="project-title">
-                                        <h3>{project.projectTitle}</h3>
+            <div className="m-3">
+                <h1 className="text-center p-5">Portfolio</h1>
+                <div className="project-section w-100">
+                    {projects.map((project, index) => {
+                        return (
+                            <div id={index} className="projects" key={index}>
+                                <a href={project.liveLink} target="_blank">
+                                    <div className="project-buffer">
+                                        <div className="project-title">
+                                            <h3>{project.projectTitle}</h3>
+                                        </div>
+                                        <div className="text-center">
+                                            <img className="project-img" src={project.imageLink} alt={project.imageAtl} />
+                                        </div>
                                     </div>
-                                    <div className="text-center">
-                                        <img className="project-img" src={project.imageLink} alt={project.imageAtl} />
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    )
-                })}
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </>
     )
--- src/pages/portfolio/Project.jsx
+++ src/pages/portfolio/Project.jsx
@@ -1,54 +1,57 @@
-import fitnessBuddy from '../../../public/images/Fitness-Buddy.png';
-import tributePage from '../../../public/images/Tribute-Page.png';
-import taskBoard from '../../../public/images/Task-Board.png';
+import fitnessBuddy from '/public/images/Fitness-Buddy.png';
+import tributePage from '/public/images/Tribute-Page.png';
+import taskBoard from '/public/images/Task-board.png';
+import restaurantApp from '/public/images/City-Restaurant-Finder.png';
+import fitnessBlog from '/public/images/Fitness-Blog.png';
+import comingSoon from '/public/images/Coming-Soon.png';
 
 export const projects = [
     {
       projectTitle: "Fitness Buddy",
       description: "An application that makes it easy to track your workouts.",
-      imageLink: {fitnessBuddy},
+      imageLink: fitnessBuddy,
       imageAtl: "Image of Fitness Buddy",
       liveLink: "https://fitness-buddy-2.onrender.com/",
       gitHub: "https://github.com/Drew-Andersen/Fitness-Buddy"
     },
     {
+        projectTitle: "City Restaurant Finder",
+        description: "Web application designed to help users explore local restaurants and the current weather in a city.",
+        imageLink: restaurantApp,
+        imageAtl: "Image of City Restaurant Finder",
+        liveLink: "https://djdevjams.github.io/city_eventtracker/",
+        gitHub: "https://github.com/Drew-Andersen/City-Restaurant-Finder"
+    },
+    {
+        projectTitle: "Task Board",
+        description: "A task board where you can move around tasks from 'To-Do', In Progress' and 'Done'.",
+        imageLink: taskBoard,
+        imageAtl: "Image of the Task Board",
+        liveLink: "https://drew-andersen.github.io/Task-Board/",
+        gitHub: "https://github.com/Drew-Andersen/Task-Board"
+    },
+    {
         projectTitle: "Tribute Page",
         description: "A tribute page to General James Roy Andersen",
-        imageLink: {tributePage},
+        imageLink: tributePage,
         imageAtl: "Picture of General Andersen",
         liveLink: "https://drew-andersen.github.io/Tribute-Page/",
         gitHub: "https://github.com/Drew-Andersen/Tribute-Page"
     },
     {
-        projectTitle: "Task Board",
-        description: "A task board where you can move around tasks from 'To-Do', In Progress' and 'Done'.",
-        imageLink: {taskBoard},
-        imageAtl: "Image of the Task Board",
-        liveLink: "https://drew-andersen.github.io/Task-Board/",
-        gitHub: "https://github.com/Drew-Andersen/Task-Board"
+        projectTitle: "Fitness Blog",
+        description: "A BLog that uses localStorage for a Fitness Blog",
+        imageLink: fitnessBlog,
+        imageAtl: "Image of Fitness Blog App",
+        liveLink: "https://drew-andersen.github.io/Fitness-Blog/",
+        gitHub: "https://github.com/Drew-Andersen/Fitness-Blog"
+    },
+    {
+        projectTitle: "Coming Soon",
+        description: "",
+        imageLink: comingSoon,
+        imageAtl: "Image of Coming Soon",
+        liveLink: "",
+        gitHub: ""
     }
-    // {
-    //     projectTitle: "",
-    //     description: "",
-    //     imageLink: {},
-    //     imageAtl: "",
-    //     liveLink: "",
-    //     gitHub: ""
-    // },
-    // {
-    //     projectTitle: "",
-    //     description: "",
-    //     imageLink: {},
-    //     imageAtl: "",
-    //     liveLink: "",
-    //     gitHub: ""
-    // },
-    // {
-    //     projectTitle: "",
-    //     description: "",
-    //     imageLink: {},
-    //     imageAtl: "",
-    //     liveLink: "",
-    //     gitHub: ""
-    // }
 ]
