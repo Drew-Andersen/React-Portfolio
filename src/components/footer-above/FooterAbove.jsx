e9f467ee33060e128907b8d1ca16f2029d4a592f
Drew-Andersen <andrewtandersen@gmail.com>
2024-10-11 09:41:21 -0400

    Push

--- src/components/footer-above/FooterAbove.jsx
+++ src/components/footer-above/FooterAbove.jsx
@@ -7,19 +7,19 @@ export default function FooterAbove() {
             <div className="contact-links d-flex flex-wrap justify-content-around text-center my-5">
                 <a href="https://github.com/Drew-Andersen" id="profile-link" className="button contact-details text-black mx-5" target="_blank">
                     <i className="fa fa-brands fa-github"></i>
-                    {/* GitHub */}
+                    <p className='display-none'>GitHub</p>
                 </a>
                 <a href="mailto:andrewtandersen@gmail.com" className="button contact-details text-black mx-5">
                     <i className="fa fa-envelope"></i>
-                    {/* Email */}
+                    <p className='display-none'>Email</p>
                 </a>
                 <a href="https://www.linkedin.com/in/andrew-andersen/" className="button contact-details text-black mx-5" target="_blank">
                     <i className="fa fa-linkedin"></i>
-                    {/* LinkedIn */}
+                    <p className='display-none'>LinkedIn</p>
                 </a>
                 <a href={Resume} className="button contact-details text-black mx-5" target="_blank">
                     <i className="fa fa-file"></i>
-                    {/* Resume */}
+                    <p className='display-none'>Resume</p>
                 </a>
             </div>
         </>
